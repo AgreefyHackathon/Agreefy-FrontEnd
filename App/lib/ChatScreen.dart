999aadaefbd4e9a5a8aff433d5eca3d4baa09446
Murad <muradshahmamadli@gmail.com>
2022-02-27 04:06:57 -0800

    Design changes

--- App/lib/ChatScreen.dart
+++ App/lib/ChatScreen.dart
@@ -1,11 +1,70 @@
-import 'package:at_chat_flutter/at_chat_flutter.dart';
+import 'package:at_chat_flutter/services/chat_service.dart' as CS;
+import 'package:hi/a_chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:open_file/open_file.dart';
+import 'dart:io';
 
-class ChatsScreen extends StatelessWidget {
+class ChatsScreen extends StatefulWidget {
   const ChatsScreen(this.title, {Key? key}) : super(key: key);
   final String title;
+
+  @override
+  State<ChatsScreen> createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+
+  File? contract;
+  late CS.ChatService _chatService;
+
+
+  void initState(){
+    _chatService = CS.ChatService();
+  }
+
+  Future<File?> _buildPopupDialog(BuildContext context) async{
+    showDialog(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(
+            builder: (context,setState) {
+              return AlertDialog(
+                title: Text('Choose a contract'),
+                content: ElevatedButton(
+                  onPressed: () async {
+                    FilePickerResult? result =
+                    await FilePicker.platform.pickFiles(allowMultiple: true);
+                    if (result != null) {
+                      setState(() {
+                        contract = File(result.files.single.path!);
+                      });
+                    }
+                  },
+                  child: Text("Upload a file"),
+                ),
+                actions: <Widget>[
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('Close')
+                  ),
+                  if(contract != null)...{
+                    TextButton(
+                        onPressed: () {
+                          _chatService.sendImageFile(context, contract!);
+                        },
+                        child: Text('Upload')
+                    ),
+                  }
+                ],
+              );
+            }
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,19 +75,21 @@ class ChatsScreen extends StatelessWidget {
             color: Colors.lightBlueAccent,
           ),
           onPressed: () async {
+            _buildPopupDialog(context);
             //this opens the up the option to choose a file
-            final result =
-                await FilePicker.platform.pickFiles(allowMultiple: true);
 
+            // if (result != null) {
+            //   File file = File(result.files.single.path!);
+            // }
             //this line opens a single file
-            final file = result?.files.first;
-            openFile(file!);
+            // final file = result?.files.first;
+            //   openFile(file!);
           },
         )
       ]),
       body: ChatScreen(
         height: MediaQuery.of(context).size.height,
-        title: title,
+        title: widget.title,
         isScreen: true,
         incomingMessageColor: Colors.green[200]!,
         outgoingMessageColor: Colors.blue[200]!,
--- App/lib/main.dart
+++ App/lib/main.dart
@@ -53,24 +53,46 @@ class _MyAppState extends State<MyApp> {
   // * load the AtClientPreference in the background
   Future<AtClientPreference> futurePreference = loadAtClientPreference();
 
+  double _mediaHeight = 0;
+  double _mediaWidth = 0;
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          backgroundColor: Colors.yellow,
-          appBar: AppBar(
-            centerTitle: true,
-            title: Text('Agreefy',
-                style: GoogleFonts.lobster(color: Colors.black, fontSize: 36)),
-            backgroundColor: Colors.blue,
-          ),
+          backgroundColor: Colors.white,
           body: Builder(
             builder: (context) => Center(
               child: Column(children: <Widget>[
-                Text(
-                    "Welcome to Agreefy! An End-to-End Encrypted app to sign lease agreenments!",
-                    style: GoogleFonts.abel(color: Colors.black, fontSize: 24)),
+                SizedBox(
+                  height: 120,
+                ),
+                Image(
+                    image: AssetImage("assets/logo.png"),
+                    width: 230,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                      "Welcome to Agreefy!",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.roboto(color: Color(0xff004c4c), fontSize: 30)),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  child: Text(
+                      "An end-to-end encrypted app to sign lease agreements!",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.roboto(color: Color(0xff004c4c), fontSize: 22)),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
                 Container(
                   child: ElevatedButton(
                     onPressed: () async {
@@ -94,13 +116,39 @@ class _MyAppState extends State<MyApp> {
                         nextScreen: const ContactScreen(),
                       );
                     },
-                    child: Text(
-                      'Create an @Sign!',
-                      style: GoogleFonts.lobster(
-                          fontSize: 36, color: Colors.black),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Color(0xff21d0b2)), shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                        )
+                    )
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 2,vertical: 4),
+                      child: Text(
+                        'Get Started',
+                        style: GoogleFonts.roboto(
+                            fontSize: 28, color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
+                Expanded(
+                    child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                                "Already have an account? Sign In",
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.grey),
+                            ),
+                          ],
+                        )
+                    )
+                ),
+                SizedBox(height: 20,)
               ]),
             ),
           ),
